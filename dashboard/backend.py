f6fbf0b2f105709187d1368416c18e4ceb03fbe5
Henri du Plessis <21668388@sun.ac.za>
2022-05-24 12:17:56 +0200

    Started work on first function

--- dashboard/backend.py
+++ dashboard/backend.py
@@ -8,7 +8,29 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM tbl_BA_Beta_Output')
+
+# Function 1
+# rDate: one of the quarter end dates corresponding to reporting dates
+# indexCode: "ALSI", "FLED", "LRGC", etc
+#
+def GetICsAndWeights(rDate, indexCode):
+	'''return a string column vector/list called ICs that contains 
+	the constituents of the respective indexCode in the same month and year 
+	as the inputted rDate.'''
+
+	return 0
+
+
+
+cursor.execute('SELECT * FROM tbl_Index_Constituents')
+
+
+# Function 2
+
+
+
+# Function 3
+
 
 data = pd.DataFrame()
 
