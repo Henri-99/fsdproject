569c9c42dc83920a866453e9eede0720e3487f21
Henri du Plessis <21668388@sun.ac.za>
2022-05-24 13:15:10 +0200

    Completed first part of Function 1

--- dashboard/backend.py
+++ dashboard/backend.py
@@ -1,6 +1,7 @@
 import pyodbc
 import numpy as np
 import pandas as pd
+import datetime as date
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=localhost\SQLEXPRESS;'
@@ -17,23 +18,47 @@ def GetICsAndWeights(rDate, indexCode):
 	'''return a string column vector/list called ICs that contains 
 	the constituents of the respective indexCode in the same month and year 
 	as the inputted rDate.'''
+	
+	query = 'SELECT * FROM tbl_Index_Constituents'
+	cursor.execute(query)
 
-	return 0
+	ICs = list()
 
+	# Find relevant column number from indexCode
+	if (indexCode == "ALSI" or indexCode == "FLED"):
+		colName = "ALSI New"
+	elif (indexCode == "LRGC" or indexCode == "MIDC" or  indexCode == "SMLC"):
+		colName = "Index New"
+	else:
+		colName = indexCode + " New"
+	print(colName)
 
+	colNo = 0
+	for col in cursor.description:
+		if (col[0] == colName):
+			break
+		colNo += 1 
 
-cursor.execute('SELECT * FROM tbl_Index_Constituents')
+	# Find alpha codes and append to ICs list
+	for record in cursor:
+
+		# Isolate records by year and month
+		recordDate = record[0]
+		if (rDate.year != recordDate.year or rDate.month != recordDate.month):
+			continue
+
+		# Isolate records by index column
+		if (record[colNo] == indexCode):
+			ICs.append(record[2])
+			#print(record[0], record[2], record[colNo])
+
+	return ICs
+
+print(GetICsAndWeights(date.datetime(2017, 9, 15), "ALTI"))
+
 
 
 # Function 2
 
 
-
 # Function 3
-
-
-data = pd.DataFrame()
-
-for i in cursor:
-	print(i)
-	wait = input()
