6aa606c9678c4febc08be25a272f33dacc9da705
Henri du Plessis <dplhen017@myuct.ac.za>
2022-06-15 17:47:15 +0200

    Added basic nav bar

---
+++ dashboard/src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+	<nav class="navbar navbar-expand-lg bg-light">
+		<div class="container-fluid">
+			<div class = "mx-auto">
+			<img alt="UCT logo" class="logo" src="src/assets/uct_logo.png" height="100" />
+			<img alt="AIFMRM logo" class="logo" src="src/assets/logo.svg" height="140" />
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Indices</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" href="#">FAQs</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" href="#">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Contact</a>
+					</li>
+					
+
+
+				</ul>
+
+			</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<style>
+.container-fluid {
+	background-color: white;
+}
+.container{
+	text-align: center;
+}
+
+.logo{
+	padding-right:1rem
+}
+
+</style>
