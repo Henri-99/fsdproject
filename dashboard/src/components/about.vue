9b794a074cc29dfc849c9260709da377c3daab21
Mohsen Elhag <mohsenelhag@outlook.com>
2022-06-17 11:11:58 +0200

    nav bar and pages

---
+++ dashboard/src/components/about.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>The About page</h1>
+</template>
+
+<script>
+	import NavBarVue from './NavBar.vue'
+	export default {
+		name: 'About',
+		components: {
+			NavBarVue
+		}
+	}
+</script>
