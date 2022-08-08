32e8878d79e27d10774a63f397d23bdedb05b5c0
Mohsen Elhag <mohsenelhag@outlook.com>
2022-08-08 13:51:15 +0200

    Added footer for home page

--- frontend/client/src/App.vue
+++ frontend/client/src/App.vue
@@ -19,9 +19,7 @@
 						</li>
 
 						<li class="nav-item  btn btn-primary">
-							<router-link :to="{ name: 'PingTest' }">
-								Index Constituents
-							</router-link>
+							
 						</li>
 
 						<li class="nav-item  btn btn-primary">
